6d177ac89b294c03d8d19936d59b5e048b3257bc
Mitake-ktm <kev800da@gmail.com>
2024-11-28 11:09:17 +0100

    first commit

---
+++ serveur.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+mongoose.connect('mongodb://localhost:27017/gestion-interventions',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => console.log('MongoDB connected'));
+
+app.listen(300, () => console.log('Server running on port 3000'));
